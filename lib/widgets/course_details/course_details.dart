f2eae4e8f304960f3949cbe0d7f10fede7c62efb
lamijahecimovic <lamijahecimovic2@gmail.com>
2025-09-02 19:02:10 +0200

    responsive text

--- lib/widgets/course_details/course_details.dart
+++ lib/widgets/course_details/course_details.dart
@@ -1,30 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class CourseDetails extends StatelessWidget {
   const CourseDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 600,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "RENT A \nCAR",
-            style: TextStyle(
-                fontWeight: FontWeight.w800, height: 0.9, fontSize: 80),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            "Iznajmljivanje automobila pruža vam potpunu slobodu i udobnost putovanja, bilo da ste na odmoru, poslovnom putu ili istražujete nove destinacije. Uz jednostavnu rezervaciju i povoljne cijene, vaše savršeno vozilo vas čeka spremno za put.",
-            style: TextStyle(fontSize: 21, height: 1.7),
-          ),
-        ],
-      ),
-    );
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      var textAlignment =
+          sizingInformation.deviceScreenType == DeviceScreenType.desktop
+              ? TextAlign.left
+              : TextAlign.center;
+      double titleSize =
+          sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 50
+              : 80;
+      double descriptionSize =
+          sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 16
+              : 21;
+
+      return Container(
+        width: 600,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "RENT A \nCAR",
+              style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  height: 0.9,
+                  fontSize: titleSize),
+              textAlign: textAlignment,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Iznajmljivanje automobila pruža vam potpunu slobodu i udobnost putovanja, bilo da ste na odmoru, poslovnom putu ili istražujete nove destinacije. Uz jednostavnu rezervaciju i povoljne cijene, vaše savršeno vozilo vas čeka spremno za put.",
+              style: TextStyle(fontSize: descriptionSize, height: 1.7),
+              textAlign: textAlignment,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
